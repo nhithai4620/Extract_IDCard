ac58055b8afc470bd5c9748eae111d6070afc046
Nguyễn Nhĩ Thái <62824044+nhithai4620@users.noreply.github.com>
2020-11-15 20:23:07 +0700

    Thai commit 15/11/2020 8:22pm
    Create functions

--- openCV.py
+++ openCV.py
@@ -2,97 +2,110 @@ import numpy as np
 import cv2
 import imutils
 import pytesseract
+#import các thư viện cần thiết
+
+def show_img(image):
+    cv2.imshow("image_name",image)
+    cv2.waitKey(0)
+#Hàm hiển thị ảnh
+
+def img_load(file_name):
+     image = cv2.imread(file_name) # load ảnh
+     image = imutils.resize(image, height = 500)  #resize ảnh 
+     return image
+#Hàm load ảnh từ file
+
+def img_retouch(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #ảnh xám
+    blur = cv2.GaussianBlur(gray, (5, 5), 0) # làm mờ ảnh
+    bilateral = cv2.bilateralFilter(blur, 5, 10, 10) # làm mịn ảnh
+    return bilateral
+#Hàm xử lí ảnh
+
+def find_contours(bilateral,image):
+    #_, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    edged = cv2.Canny(bilateral, 30, 120) # tìm cạnh bằng Canny
+    contours, _ = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # đường viền
+    largest_area = sorted(contours, key = cv2.contourArea)
+
+    for cnts in largest_area:
+        peri = cv2.arcLength(cnts, True)
+        approx = cv2.approxPolyDP(cnts, 0.09 * peri, True)
+
+    cv2.drawContours(image, [approx], 0, (0, 255, 255), 5) # vẽ đường viền
+    return approx # trả về đỉnh
+#Hàm tìm đường viền
+
+def crop_contours(approx, image):
+    coordinates_0 = (approx[0,0,0], approx[0,0,1])
+    coordinates_1 = (approx[1,0,0], approx[1,0,1])
+    coordinates_2 = (approx[2,0,0], approx[2,0,1])
+    coordinates_3 = (approx[3,0,0], approx[3,0,1])
+
+    if approx[0,0,0] < 320:
+        top_left = coordinates_0
+        bottom_left = coordinates_1
+        bottom_right = coordinates_2
+        top_right = coordinates_3
+
+    else:
+        top_right = coordinates_0
+        top_left = coordinates_1
+        bottom_left = coordinates_2
+        bottom_right = coordinates_3
+
+    pts1 = np.float32([[top_left], [top_right], [bottom_right], [bottom_left]])
+    pts2 = np.float32([[0, 0], [450, 0], [450, 300], [0, 300]])
+    matrix = cv2.getPerspectiveTransform(pts1, pts2)
+    result = cv2.warpPerspective(image, matrix, (450, 300))
+    return result
+#Hàm cắt các đường viền
+
+def crop_cmnd(result):
+    img_cmnd = result[70:103, 225:390]
+    return img_cmnd
+#Hàm cắt trường cmnd
+
+def crop_hoten(result):
+    img_hoten = result[100:150, 187:420]
+    return img_hoten
+#Hàm cắt trường họ tên
+
+def char_recog(img):
+    custom_config = r'-c tessedit_char_whitelist=0123456789' #cấu hình
+    text_recog = pytesseract.image_to_string(img, lang='eng',config=custom_config)#nhận dạng
+    return text_recog #trả về kiểu test
+
+def main():
+    image = img_load('./Image/cmnd5.jpg')
+    bilateral = img_retouch(image)
+    approx = find_contours(bilateral,image)
+    result = crop_contours(approx, image)
+    show_img(result)
+    so_cmnd = crop_cmnd(result)
+    show_img(so_cmnd)
+    ho_ten = crop_hoten(result)
+    show_img(ho_ten)
+    so_cmnd_retouch = img_retouch(so_cmnd)
+    show_img(so_cmnd_retouch)
+    ho_ten_retouch = img_retouch(ho_ten)
+    show_img(ho_ten_retouch)
+    print('So cmnd : ', char_recog(so_cmnd_retouch))
+    print('Ho va ten : ', char_recog(ho_ten_retouch))
+
+    
+main()
 
 
-image = cv2.imread('cmnd5.jpg')
-image = imutils.resize(image, height = 500)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #ảnh xám
-blur = cv2.GaussianBlur(gray, (5, 5), 0) # làm mờ ảnh
-bilateral = cv2.bilateralFilter(blur, 5, 10, 10) # làm mịn ảnh
 
+# # # def edit_image_after(image):
+# # #     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# # #     blur_image = cv2.GaussianBlur(gray_image, (3, 3), 1)
+# # #     bilateral_image = cv2.bilateralFilter(blur_image, 5, 10, 10)
+# # #     # thresh_ho_ten = cv2.adaptiveThreshold(bilateral_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 3, 3)
+# # #     # _, thresh_ho_ten = cv2.threshold(bilateral_image ,125,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 
-# _, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-edged = cv2.Canny(bilateral, 30, 120) # tìm cạnh bằng Canny
+# # #     return bilateral_image
 
-contours, _ = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # đường viền
-
-largest_area = sorted(contours, key = cv2.contourArea)
-
-for cnts in largest_area:
-    peri = cv2.arcLength(cnts, True)
-    approx = cv2.approxPolyDP(cnts, 0.09 * peri, True)
-
-print(approx)
-cv2.drawContours(image, [approx], 0, (0, 255, 255), 5) 
-
-cv2.imshow('Image', image)
-cv2.waitKey(0)
-
-coordinates_0 = (approx[0,0,0], approx[0,0,1])
-coordinates_1 = (approx[1,0,0], approx[1,0,1])
-coordinates_2 = (approx[2,0,0], approx[2,0,1])
-coordinates_3 = (approx[3,0,0], approx[3,0,1])
-
-if approx[0,0,0] < 320:
-    top_left = coordinates_0
-    bottom_left = coordinates_1
-    bottom_right = coordinates_2
-    top_right = coordinates_3
-
-else:
-    top_right = coordinates_0
-    top_left = coordinates_1
-    bottom_left = coordinates_2
-    bottom_right = coordinates_3
-
-pts1 = np.float32([[top_left], [top_right], [bottom_right], [bottom_left]])
-pts2 = np.float32([[0, 0], [450, 0], [450, 300], [0, 300]])
-matrix = cv2.getPerspectiveTransform(pts1, pts2)
-result = cv2.warpPerspective(image, matrix, (450, 300))
-
-
-cv2.imshow('Output', result)
-cv2.waitKey(0)
- 
-so_cmnd = result[70:103, 225:390]
-ho_ten = result[100:150, 187:420] # cắt ảnh
-
-cv2.imshow('so_cmnd', so_cmnd)
-cv2.waitKey(0)
-cv2.imshow('hoten', ho_ten)
-cv2.waitKey(0)
-
-
-
-
-# def edit_image_after(image):
-#     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#     blur_image = cv2.GaussianBlur(gray_image, (3, 3), 1)
-#     bilateral_image = cv2.bilateralFilter(blur_image, 5, 10, 10)
-#     # thresh_ho_ten = cv2.adaptiveThreshold(bilateral_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 3, 3)
-#     # _, thresh_ho_ten = cv2.threshold(bilateral_image ,125,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-
-#     return bilateral_image
-
-gray_so_cmnd = cv2.cvtColor(so_cmnd, cv2.COLOR_BGR2GRAY)
-blur_so_cmnd = cv2.GaussianBlur(gray_so_cmnd, (3, 3), 1)
-bilateral_so_cmnd = cv2.bilateralFilter(blur_so_cmnd, 5, 10, 10)
-# thresh_so_cmnd = cv2.adaptiveThreshold(bilateral_so_cmnd, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 5, 4)
-cv2.imshow('socmnd', bilateral_so_cmnd)
-cv2.waitKey(0)
-
-gray_ho_ten = cv2.cvtColor(ho_ten, cv2.COLOR_BGR2GRAY)
-blur_ho_ten = cv2.GaussianBlur(gray_ho_ten, (3, 3), 1)
-bilateral_ho_ten = cv2.bilateralFilter(blur_ho_ten, 5, 10, 10)
-cv2.imshow('hoten', bilateral_ho_ten)
-cv2.waitKey(0)
-
-custom_config = r'-c tessedit_char_whitelist=0123456789' # cấu hình
-so_cmnd_text = pytesseract.image_to_string(bilateral_so_cmnd, lang='vie',
-                                config=custom_config)
-print('So CMND: ' + so_cmnd_text)
-
-ho_ten_text = pytesseract.image_to_string(bilateral_ho_ten, lang='vie')
-print('Ho va ten: ' + ho_ten_text)
 
 
